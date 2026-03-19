d9492153e2666c88ffc57e6a2b4e3b01d808dc1c
kai <ne241092@senshu-u.jp>
2026-03-20 08:52:53 +0900

    Updatepost　リポジトリ

--- repository/post_repository.go
+++ repository/post_repository.go
@@ -11,4 +11,5 @@ type PostRepository interface {
 	GetPost(ctx context.Context, id int64) (*model.Post, error)
 	GetPostsByAuthorID(ctx context.Context, authorID int64) ([]*model.Post, error)
 	GetAllPosts(ctx context.Context) ([]*model.Post, error)
+	UpdatePost(ctx context.Context) ([]*model.Post) error
 }
