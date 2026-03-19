811e8ac30d79c338ed17be8095bce9d7a2a56a53
kai <ne241092@senshu-u.jp>
2026-03-20 03:57:44 +0900

    CreatePost　モデル検討

---
+++ model/post.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"time"
+)
+
+type Post struct {
+	ID        int64     `json:"id"`
+	AuthorID  int64     `json:"author_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
