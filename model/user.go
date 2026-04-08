a750f667199696b204fcd2a00429538d7f888d42
kai <ne241092@senshu-u.jp>
2026-04-08 14:24:24 +0900

    アカウントモデル作成

---
+++ model/user.go
@@ -0,0 +1,51 @@
+packgage model
+
+import (
+	"time"
+	"errors"
+)
+
+type User struct {
+	ID       int64  `json:"id"`
+	UserID   string `json:"user_id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	HashedPassword string `json:"hashed_password"`
+	Role     string `json:"role"`
+	Status   string `json:"status"`
+	CreatedAt int64  `json:"created_at"`
+	UpdatedAt int64  `json:"updated_at"`
+}
+
+type CreateUserParam struct {
+	UserID   string `json:"user_id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func hashPassword(password string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedBytes), nil
+}
+
+func (u *User) CreateUser(param CreateUserParam) error {
+	hashedPassword, err := hashPassword(param.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	u.UserID = param.UserID
+	u.Name = param.Name
+	u.Email = param.Email
+	u.HashedPassword = hashedPassword
+	u.CreatedAt = param.CreatedAt
+	u.UpdatedAt = param.UpdatedAt
+
+	return nil
+}
