04f8698032d45854b2f1802eb0dcf473387126f2
ne241099c <ne241099@senshu-u.jp>
2026-05-29 00:48:59 +0900

    やべ

--- db/migrations/009_create_profiles.up.sql
+++ db/migrations/009_create_profiles.up.sql
@@ -1,10 +1,8 @@
-DROP TABLE IF EXISTS profiles;
-
-CREATE TABLE profiles (
-    user_id BIGINT PRIMARY KEY,   
+CREATE TABLE IF NOT EXISTS profiles (
+    user_id BIGINT PRIMARY KEY,
     bio TEXT,
     avatar_key VARCHAR(255) DEFAULT NULL,
     created_at BIGINT NOT NULL,
     updated_at BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
